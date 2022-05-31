6f6daba01bbf81ccb7b762c5324dcacdf8cb6f09
Leovihildo <46786869+Leovihildo@users.noreply.github.com>
2022-05-31 20:11:45 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// const Swal = require('sweetalert2');
+const Swal = require('sweetalert2');
 
 // Swal.fire("hello world!");
 var tasks = [];
@@ -30,8 +30,26 @@ function create(){
 }
 
 function Delete(item){
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
     tasks.splice(item, 1);
     create();
+    Swal.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  }
+})
+    
 
 }
 
